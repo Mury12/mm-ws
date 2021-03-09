f035a015e84604c2069a693fc0b95ba7059b49ce
Andre Mury <mury_gh@hotmail.com>
2021-03-09 14:20:39 -0300

    fix: mvc-creator script dependencies

--- mvc-creator.php
+++ mvc-creator.php
@@ -5,9 +5,19 @@
  */
 
 use MMWS\Handler\DatabaseModelExtractor;
-require_once 'app/autoload.php';
+use Dotenv\Dotenv;
 
-require_once('app/config/db-conf.php');
+/** Composer autoload */
+require_once __DIR__ . '/vendor/autoload.php';
+require_once './app/autoload.php';
+
+/**
+ * @var Dotenv\Dotenv $dotenv loads the environment variables in .env
+ */
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require_once 'app/config/db-conf.php';
 
 $dbm = new DatabaseModelExtractor(DB_NAME, 'app/partials/classes', 1);
 
