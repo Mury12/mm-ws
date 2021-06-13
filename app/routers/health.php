6cd0035788ee1bcecce6653f0ea41980ff376dbe
André Mury <mury_gh@hotmail.com>
2021-06-13 19:02:57 -0300

    added a helloworld sql for first test and route examples

--- app/routers/health.php
+++ app/routers/health.php
@@ -1,5 +1,26 @@
 <?php
 
+/**
+ * Example of Usage
+ * ```php
+ * return [
+ *  'user' => [
+ *		'params' => ['id'], # it will be accessed at $this->data['params']['id'] in your Module file
+ *		'body' => EndpointFactory::create()
+ *			->post('user/manage', 'create')
+ *			->get('user/manage', 'get')
+ *			->put('user/manage', 'update')
+ *			->delete('user/manage', 'delete'),
+ *		// Add children routes calling the http methods from endpoint
+ *		'another-children-route' => [
+ *			'body' => EndpointFactory::create()
+ *				->get('user/manage', 'exampleMethod'),
+ *		]
+ *	 ],
+ * ]
+ * ```
+ */
+
 use MMWS\Factory\EndpointFactory;
 
 return [
--- app/routers/ws/v2.php
+++ app/routers/ws/v2.php
@@ -1,51 +1,28 @@
 <?php
 
 /**
- * Routes are now defined outside main router file.
- * Now its possible to add params in the current layout
- * index, such as:
- *  'route-name' => [
- *      'params' => ['param1', 'param2'],
- *      'body'   => $l = new Endpoint(),
- *                  $l->page('domain/page')
- *                    ->permission('auth')
- *  ],
- * 
- * Params index MUST BE before body properties, otherwise, it will be lost.
+ * Example of Usage
+ * ```php
+ * return [
+ *  'user' => [
+ *		'params' => ['id'], # it will be accessed at $this->data['params']['id'] in your Module file
+ *		'body' => EndpointFactory::create()
+ *			->post('user/manage', 'create')
+ *			->get('user/manage', 'get')
+ *			->put('user/manage', 'update')
+ *			->delete('user/manage', 'delete'),
+ *		// Add children routes calling the http methods from endpoint
+ *		'another-children-route' => [
+ *			'body' => EndpointFactory::create()
+ *				->get('user/manage', 'exampleMethod'),
+ *		]
+ *	 ],
+ * ]
+ * ```
  */
 
 use MMWS\Factory\EndpointFactory;
 
 return [
-	'wine' => [
-		'params' => ['codigo'],
-		'body' => EndpointFactory::create()
-			->post('wine/manage', 'create')
-			->get('wine/manage', 'get')
-			->put('wine/manage', 'update')
-			->delete('wine/manage', 'delete')
-			->cache(),
-		// Add children routes calling the http methods from endpoint
-		'another-children-route' => [
-			'body' => EndpointFactory::create()
-				->get('wine/manage', 'exampleMethod'),
-		]
-	],
-	'manage' => [
-		'params' => ['userId'],
-		'body' => EndpointFactory::create()
-			->post('user/manage', 'create')
-			->get('user/manage', 'get')
-			->put('user/manage', 'update')
-			->delete('user/manage', 'delete'),
-		// Add children routes calling the http methods from endpoint
-		'another-children-route' => [
-			'params' => ['id'],
-			'body' => EndpointFactory::create()
-				->post('user/manage', 'create')
-				->get('user/manage', 'get')
-				->put('user/manage', 'update')
-				->delete('user/manage', 'delete'),
-		]
-	],
+
 ];
